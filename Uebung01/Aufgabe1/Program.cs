7a9a51f06cbb2ce1e14f289689a805c5f3cbc5eb
bpaulwitz <baldur.paulwitz@web.de>
2021-05-18 13:03:12 +0200

    Übung_21_05_18

---
+++ Uebung01/Aufgabe1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Aufgabe1
+{
+    class Program
+    {
+        static void Main1_1()
+        {
+            double centProMin = 9.9;
+            double gebuehr = 399;
+            double freiMinuten = 100;
+
+            double anzahlMinuten = 101;
+            double kosten = gebuehr;
+
+            if(anzahlMinuten > freiMinuten)
+            {
+                kosten += centProMin * (anzahlMinuten - freiMinuten);
+            }
+
+            //oder mit ternären Operator:
+            //kosten += anzahlMinuten > freiMinuten ? centProMin * (anzahlMinuten - freiMinuten) : 0;
+
+            Console.WriteLine($"Kosten: {(kosten / 100.0):0.00} Euro");
+        }
+
+        static void Main1_2()
+        {
+            double centProMin = 9.9;
+            double gebuehr = 399;
+            double freiMinuten = 100;
+            double anzahlMinuten;
+
+            Console.WriteLine("Anzahl der Minuten angeben:");
+
+            //Wenn der Code im if-Statement nur eine Zeile lang ist, können die geschweiften Klammern darum weggelassen werden
+            if(!Double.TryParse(Console.ReadLine(), out anzahlMinuten))
+            {
+                Console.WriteLine("not a valid number");
+                return;
+            }
+                            
+            double kosten = gebuehr;
+
+            if(anzahlMinuten > freiMinuten)
+            {
+                kosten += centProMin * (anzahlMinuten - freiMinuten);
+            }
+
+            //oder mit ternären Operator:
+            //kosten += anzahlMinuten > freiMinuten ? centProMin * (anzahlMinuten - freiMinuten) : 0;
+
+            Console.WriteLine($"Kosten: {(kosten / 100.0):0.00} Euro");
+        }
+
+        static void Main1_3()
+        {
+            double centProMin = 9.9;
+            double gebuehr = 399;
+            double tarifSGebuehr = 600;
+            double tarifMGebuehr = 1100;
+            double tarifLGebuehr = 2000;
+
+            double freiMinuten = 100;
+            double anzahlMinuten;
+            char tarif;
+
+            Console.WriteLine("Anzahl der Minuten angeben:");
+
+            //Wenn der Code im if-Statement nur eine Zeile lang ist, können die geschweiften Klammern darum weggelassen werden
+            if(!Double.TryParse(Console.ReadLine(), out anzahlMinuten))
+            {
+                Console.WriteLine("not a valid number");
+                return;
+            }
+
+            Console.WriteLine("Tarif [S | M | L] angeben:");
+            tarif = Console.ReadKey().KeyChar;
+            if(tarif != 'S' && tarif != 'M' && tarif != 'L')
+            {
+                Console.WriteLine("not a valid option");
+                return;
+            }
+            
+            double kosten = gebuehr;
+            kosten += anzahlMinuten > freiMinuten ? centProMin * (anzahlMinuten - freiMinuten) : 0;
+
+            switch (tarif)
+            {
+                case 'S':
+                    kosten += tarifSGebuehr;
+                    break;
+                case 'M':
+                    kosten += tarifMGebuehr;
+                    break;
+                case 'L':
+                    kosten += tarifLGebuehr;
+                    break;
+            }
+
+            Console.WriteLine($"\nKosten: {(kosten / 100.0):0.00} Euro");
+        }
+
+        static void Main(string[] args)
+        {
+            Main1_3();
+        }
+    }
+}
---
+++ Uebung01/Aufgabe2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Aufgabe2
+{
+    class Program
+    {
+        static void Main2_1()
+        {
+            Console.WriteLine("Jahr eingeben:");
+            int jahr;
+            if(!int.TryParse(Console.ReadLine(), out jahr))
+            {
+                Console.WriteLine("Kein gültiges Jahr!");
+                return;
+            }
+            
+            bool vierTeilbar = jahr % 4 == 0;
+            if(vierTeilbar)
+            {
+                bool saekularJahr = jahr % 100 == 0;
+                if(saekularJahr)
+                {
+                    if(jahr % 400 == 0)
+                        Console.WriteLine($"{jahr} ist ein Schaltjahr");
+                    else
+                        Console.WriteLine($"{jahr} ist kein Schaltjahr");
+                }
+                else
+                    Console.WriteLine($"{jahr} ist ein Schaltjahr");
+            }
+            else
+                Console.WriteLine($"{jahr} ist kein Schaltjahr");
+        }
+
+        static void Main2_2()
+        {
+            Console.WriteLine("Jahr eingeben:");
+            string consoleInput;
+            int jahr;
+
+            //do-while Schleife wird mindestens einmal ausgeführt und testet unten in dem While-Statement, ob 
+            //die Schleife weiterhin wiederholt wird
+            do
+            {
+                consoleInput = Console.ReadLine();
+                if(consoleInput == "")
+                {
+                    //springt zum nächsten loop -> testet, ob die schleife wiederholt wird
+                    continue;
+                    //Alternativ könnte man auch 'break;' verwenden, das würde die Schleife direkt verlassen
+                }
+
+                if(!int.TryParse(consoleInput, out jahr))
+                {
+                    Console.WriteLine("Kein gültiges Jahr!");
+                    return;
+                }
+                
+                bool vierTeilbar = jahr % 4 == 0;
+                if(vierTeilbar)
+                {
+                    bool saekularJahr = jahr % 100 == 0;
+                    if(saekularJahr)
+                    {
+                        if(jahr % 400 == 0)
+                            Console.WriteLine($"{jahr} ist ein Schaltjahr");
+                        else
+                            Console.WriteLine($"{jahr} ist kein Schaltjahr");
+                    }
+                    else
+                        Console.WriteLine($"{jahr} ist ein Schaltjahr");
+                }
+                else
+                    Console.WriteLine($"{jahr} ist kein Schaltjahr");
+            } while (consoleInput != "");
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Main2_2();
+        }
+    }
+}
